92778e817c5d30c263eeb5168ce70114a0bbad54
forestfox08 <j.rdb08@hotmail.com>
2025-09-24 15:09:28 +0200

    --- 24 09 2025  ---

--- Assets/Scripts/Enemies/EnemySpawning.cs
+++ Assets/Scripts/Enemies/EnemySpawning.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 public class EnemySpawning : MonoBehaviour
 {
 
-    [Header("References")]
+    [Header("Enemy Prefabs")]
     [SerializeField] private GameObject[] enemyPrefabs;
 
     [Header("Attributes")]
     [SerializeField] private int enemybaseAmount = 5;
     [SerializeField] private float enemiesPerSecond = 0.5f;
+
+    //------------------
+
+    [SerializeField] private float timeSinceSpawn;
+    public float CurrentWave = 1f;
     public float timebetweenWaves = 20f;
+    private int enemiesLeftOver;
+    private int enemiesLeftToSpawn;
 
-
-
-    void Update()
+    private void Start()
     {
-        
+        enemiesLeftToSpawn = enemybaseAmount;
+    }
+    private int EnemiesPerWave()
+    {
+        return 
     }
 }
